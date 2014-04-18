cfb3169c6b081a7599cc3fc623eb93f56f6e759f
Francisco Sanz <francisco.sanz.g@gmail.com>
2014-04-18 19:53:48 +0200

    working with montogomery factorial

--- montgomery/montgomeryfactorial.c
+++ montgomery/montgomeryfactorial.c
@@ -33,11 +33,13 @@ void find_r1n1(int *n, int * r, int *n1, int *r1){
   }
 }
 
+
 int main(){
   int n=101; /* The prime number */
   int r=128; /* 2^7 */
   int n1, r1;
-  int m,t;
+  int m,t,i;
+  int t1, t2, tt;
 
   /* We've to found n1, r1 such as
    * r*r1 - n*n1 = 1 */
@@ -46,10 +48,22 @@ int main(){
 
   /* First 100*99 (mod 101) */
   m = ((100*99 & (r-1))*n1 &(r-1));
-  t = ((100*99+m*n))/128; /* This divission can be improved */
-  printf("t=%d\n",t);
+  t = ((100*99+m*n))>>7; /* 128=2^7 */
+  if(t>101)
+    t=t-101;
+  printf("t1=%d\n",t);
   printf("mod=%d\n",t*r%n);
+  t1=t;
 
+  m = ((99*98 & (r-1))*n1 &(r-1));
+  t = ((99*98+m*n))>>7; /* 128=2^7 */
+  if(t>101)
+    t=t-101;
+  printf("t2=%d\n",t);
+  printf("mod=%d\n",t*r%n);
+  t2=t;
+  tt = t1*t2 & (r-1);
+  printf("this is not working mod=%d\n",tt*r%n);
   return 0;
 }
 
